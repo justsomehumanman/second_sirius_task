4b1f0eaeb06f8470a7deb8e527f48f9a266110b6
Matvey <itstoomymail@mail.ru>
2021-10-15 14:47:25 +0500

    Add objects

--- shop_qraphql/admin.py
+++ shop_qraphql/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Seller
+from .models import *
 
 admin.site.register(Seller)
+admin.site.register(Bouquet)
+admin.site.register(Purchase)
+admin.site.register(Customer)
