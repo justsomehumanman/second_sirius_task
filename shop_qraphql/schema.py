6291bcd1cb52110972c22739b4904f85bbffc86e
Matvey <itstoomymail@mail.ru>
2021-10-15 14:44:47 +0500

    Add objects

--- shop_qraphql/schema.py
+++ shop_qraphql/schema.py
@@ -1,6 +1,21 @@
 import graphene
-from graphene_django import DjangoObjectType
+from graphene_django.types import DjangoObjectType, ObjectType
 from .models import *
+
+
+class BouquetType(DjangoObjectType):
+    class Meta:
+        model = Bouquet
+
+
+class PurchaseType(DjangoObjectType):
+    class Meta:
+        model = Purchase
+
+
+class CustomerType(DjangoObjectType):
+    class Meta:
+        model = Customer
 
 
 class SellerType(DjangoObjectType):
@@ -8,17 +23,33 @@ class SellerType(DjangoObjectType):
         model = Seller
 
 
-class SellerInput(graphene.InputObjectType):
-    id = graphene.ID()
-    shop_name = graphene.String()
-    link_on_photo = graphene.String()
-    sold_bouquets_counter = graphene.Int()
-
-
 class Query(graphene.ObjectType):
+    purchase = graphene.Field(PurchaseType, id=graphene.Int())
+    bouquet = graphene.Field(BouquetType, id=graphene.Int())
+    customer = graphene.Field(CustomerType, id=graphene.Int())
     seller = graphene.Field(SellerType, id=graphene.Int())
 
+    purchases = graphene.List(PurchaseType, id=graphene.Int())
+    bouquets = graphene.List(BouquetType)
     sellers = graphene.List(SellerType)
+
+    @staticmethod
+    def resolve_purchase(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Purchase.objects.get(pk=id)
+
+        return None
+
+    @staticmethod
+    def resolve_bouquet(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Bouquet.objects.get(pk=id)
+
+        return None
 
     @staticmethod
     def resolve_seller(self, info, **kwargs):
@@ -30,8 +61,57 @@ class Query(graphene.ObjectType):
         return None
 
     @staticmethod
+    def resolve_customer(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Customer.objects.get(pk=id)
+
+        return None
+
+    @staticmethod
     def resolve_sellers(self, info, **kwargs):
         return Seller.objects.all()
+
+    @staticmethod
+    def resolve_bouquets(self, info, **kwargs):
+        return Bouquet.objects.all()
+
+    @staticmethod
+    def resolve_purchases(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id is not None:
+            return Purchase.objects.filter(customer=id)
+
+        return None
+
+
+class BouquetInput(graphene.InputObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    price = graphene.Int()
+    link_on_photo = graphene.String()
+    seller_id = graphene.Int()
+
+
+class SellerInput(graphene.InputObjectType):
+    id = graphene.ID()
+    shop_name = graphene.String()
+    link_on_photo = graphene.String()
+    sold_bouquets_counter = graphene.Int()
+
+
+class PurchaseInput(graphene.InputObjectType):
+    id = graphene.ID()
+    bouquet_id = graphene.Int()
+    customer_id = graphene.Int()
+    cost = graphene.Int()
+    service_income = graphene.Int()
+
+
+class CustomerInput(graphene.InputObjectType):
+    name = graphene.String()
+    mail = graphene.String()
 
 
 class CreateSeller(graphene.Mutation):
